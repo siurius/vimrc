94d4c008290f183390ffde95043af4d96c49c7de
Yiran Li <yiranli91@gmail.com>
2026-02-15 16:13:59 +0800

    remove unused markdown preview

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -506,19 +506,6 @@ return {
     end,
   },
 
-  -- Markdown preview
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = "markdown",
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    keys = {
-      { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Markdown preview" },
-    },
-  },
-
   -----------------------------------------------------------------------------
   -- CODING TOOLS
   -----------------------------------------------------------------------------
